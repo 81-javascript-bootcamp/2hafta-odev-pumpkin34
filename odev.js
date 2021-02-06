7eae9fb5126c612f1c93bd636e6d730ce5b0d464
pumpkin34 <burakalparsln@gmail.com>
2021-02-06 21:36:05 +0300

    3.soru cevap

--- odev.js
+++ odev.js
@@ -2,8 +2,6 @@
 
 1. mycarDetails fonksiyonunu "GA12345 Toyota" ciktisini verecek sekilde duzenleyin.
 
-**/
-
 var car = { 
     registrationNumber: "GA12345",
     brand: "Toyota",
@@ -37,20 +35,34 @@ function isValidName(name) {
 
 
 3. summary fonkisyonunu ciktisi "Brave New World was written by Aldous Huxley. It is a dystopian novel written in 1932." olacak sekilde cagirin.
-
 **/
-/*
+/**1. Yöntemimim apply ile array kullanmak */
 const book = {
   title: 'Brave New World',
-  author: 'Aldous Huxley',
+  author: 'Aldous Huxley'
+  
 }
-
 function summary(genre, year) {
   console.log(
     `${this.title} was written by ${this.author}. It is a ${genre} novel written in ${year}.`,
   )
 }
 
+summary.apply(book,["dystopian", "1932"]);
+
+/**2. Yöntemimim eksik parametreleri book içine ekleyip oradan çağırmak 
+
+const book = {
+  title: 'Brave New World',
+  author: 'Aldous Huxley',
+  genre: 'dystopian',
+  year: 1932
+}
+function summary(genre, year) {
+  console.log(
+    book.title + "was written by " + book.author + " It is a " + book.genre 
+    +  " novel written in " + book.year)
+}
+
+summary();
 */
-
-
