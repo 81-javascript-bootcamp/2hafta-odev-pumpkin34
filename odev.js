bdd7309e0ccbc8967229c5ef9e7b907d90fd7a54
pumpkin34 <burakalparsln@gmail.com>
2021-02-06 22:32:44 +0300

    2. soru_cevap

--- odev.js
+++ odev.js
@@ -23,20 +23,27 @@ name string olmali
 name bos olmamali
 bosluk //icerebilir//, ancak bosluk haridcindeki isimler en az 2 karakterden olusmali.
 
-**/
-/*
-function isValidName(name) {
-  your code here
-}
-*/
+**/ 
 
+function isValidName(name) {
+  return typeof name == 'string' && name !== "" && name.trim().length > 1 
+}
+
+console.log(isValidName("Ahmet") === true);
+console.log(isValidName(false) === false);
+console.log(isValidName(null) === false); /// burada kontrole gerek yok çünkü type String diye check ettik.
+console.log(isValidName(undefined) === false); /// burada kontrole gerek yok çünkü type String diye check ettik.
+console.log(isValidName("") === false);
+console.log(isValidName("  \t\n") === false);
+console.log(isValidName("X") === false);
 
 /**
 
 
-3. summary fonkisyonunu ciktisi "Brave New World was written by Aldous Huxley. It is a dystopian novel written in 1932." olacak sekilde cagirin.
+3. summary fonkisyonunu ciktisi "Brave New World was written by Aldous Huxley. 
+It is a dystopian novel written in 1932." olacak sekilde cagirin.
 **/
-/**1. Yöntemimim apply ile array kullanmak */
+/**1. Yöntemimim apply ile array kullanmak 
 const book = {
   title: 'Brave New World',
   author: 'Aldous Huxley'
@@ -49,7 +56,7 @@ function summary(genre, year) {
 }
 
 summary.apply(book,["dystopian", "1932"]);
-
+*/
 /**2. Yöntemimim eksik parametreleri book içine ekleyip oradan çağırmak 
 
 const book = {
@@ -58,11 +65,13 @@ const book = {
   genre: 'dystopian',
   year: 1932
 }
-function summary(genre, year) {
+function summary(book) {
   console.log(
     book.title + "was written by " + book.author + " It is a " + book.genre 
     +  " novel written in " + book.year)
 }
 
-summary();
+summary(book);
 */
+
+
